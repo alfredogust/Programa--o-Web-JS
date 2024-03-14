58e6b39a543dfeef0ccc95e58583d49e8381dc36
alfredogust <alfredog.fu@gmail.com>
2024-03-14 17:49:19 -0300

    feat: Adicionado arquivo variaveisOperadores.js com operações matemáticas entre numA e numB

---
+++ atividade01/varOperadores.js
@@ -0,0 +1,14 @@
+const numA = 2;
+const numB = 3;
+
+const soma = numA + numB;
+const subtracao = numA - numB;
+const multiplicacao = numA * numB;
+const divisao = numA / numB;
+const resto = numA % numB;
+
+console.log(`Resultado da soma entre ${numA} e ${numB}: ${soma}`);
+console.log(`Resultado da subtração entre ${numA} e ${numB}: ${subtracao}`);
+console.log(`Resultado da multiplicação entre ${numA} e ${numB}: ${multiplicacao}`);
+console.log(`Resultado da divisão entre ${numA} e ${numB}: ${divisao}`);
+console.log(`Resto da divisão entre ${numA} e ${numB}: ${resto}`);
